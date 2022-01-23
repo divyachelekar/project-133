8f443ed92507620fd02e6ec6767e843a4aecfb30
divyachelekar <87216987+divyachelekar@users.noreply.github.com>
2022-01-23 22:31:58 +0530

    Add files via upload

---
+++ Tv and ac .js
@@ -0,0 +1,57 @@
+img="";
+status="";
+object=[];
+
+function preload(){
+    img = loadImage('Tv and ac.jpg');
+}
+
+
+function setup(){
+    canvas = createCanvas(800 , 500);
+    canvas.position(400 , 230)
+    objectDetector = ml5.objectDetector('cocossd' , modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects " ;
+
+    
+}
+function modelLoaded(){
+    console.log("Model Loaded");
+    status = true;
+    objectDetector.detect(img , gotResult);
+}
+
+function gotResult(error , results){
+
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
+    object = results;
+
+}
+
+function draw(){
+    image(img , 0 , 0 , 800 , 500);
+    if(status != ""){
+
+        for(i=0; i<object.length; i++){
+            document.getElementById("status").innerHTML = "Status : Object Detected";
+             fill('#FF0000');
+             precent = floor(object[i].confidence * 100);
+             text(object[i].label + " "+ precent + "%" , object[i].x +100 ,  object[i].y+100);
+             noFill();
+             stroke('#FF0000');
+             rect(object[i].x+100 , object[i].y+100 , object[i].width+100 , object[i].height+100);
+
+        }
+
+        
+        
+    }
+    
+}
+
+function back(){
+    window.location = "index.html";
+}
